aa3e4eebc2e9fe34fa4d72f25ae249d5130cb427
Jenny Fung <jfung@azavea.com>
2015-11-04 12:52:58 -0500

    Working Median and Start

--- PQ2.py/QuicksortGood.py
+++ PQ2.py/QuicksortGood.py
@@ -1,12 +1,11 @@
 __author__ = 'jfung'
 
 import numpy
-import statistics
 
 # Save ints to array
 list = []
 intList = []
-with open('QuickSort.txt', 'r') as f:
+with open('test.txt', 'r') as f:
     list = f.read().splitlines()
     intList = [int(c) for c in list]
 f.close()
@@ -26,10 +25,8 @@ def quickSort(arr, start, end):
 
 
 def partition(array, start, end):
-    if len(array[start:end]) > 2:
-        pivot = findMedian(array, start, end)
-    else:
-        pivot = start
+    pivot = start
+    #swap pivot element to start if not
     if pivot != start:
         array[pivot], array[start] = array[start], array[pivot]
     #update count
@@ -42,23 +39,11 @@ def partition(array, start, end):
             array[j], array[i] = array[i], array[j]
             i += 1
     array[start], array[i-1] = array[i-1], array[start]
+    print "After: {} Pivot: {} ({})".format(array[start:end], pivot, array[pivot])
+    print ""
     return i-1
 
 
-
-
-def findMedian(arr, start, end):
-    cutArray = arr[start:end]
-    midpt = len(arr[0:start]) + (len(cutArray)-1)//2
-    medianArray = [arr[start], arr[midpt], arr[end-1]]
-    median = statistics.median(medianArray)
-    if median == arr[start]:
-        pivot = start
-    elif median == arr[end-1]:
-        pivot = end-1
-    else:
-        pivot = midpt
-    return pivot
-
 quickSort(intList, 0, len(intList))
 print globalCount
+print intList
